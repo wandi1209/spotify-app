e8310caa3e1e83406020502196bde0d763fbac97
wandi1209 <wandider@gmail.com>
2025-04-06 03:12:18 +0800

    chore: initialize Flutter project structure for Spotify app

---
+++ android/app/src/main/kotlin/wandidev/com/spotify/MainActivity.kt
@@ -0,0 +1,5 @@
+package wandidev.com.spotify
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: Container(),
+    );
+  }
+}
